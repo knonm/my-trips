0f76f0a702f21f6e88e63ba64e962e68157b3b39
Matheus Canon <matheus.canon@gmail.com>
2016-05-03 01:48:53 -0300

    Faltaram algumas coisas no commit anterior

--- MyTripsClient/build.sh
+++
@@ -1,4 +0,0 @@
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-ant compile
-ant run
--- MyTripsServer/build.sh
+++
@@ -1,7 +0,0 @@
-export CATALINA_HOME=/opt/apache-tomcat-8.0.33
-export CATALINA_BASE=/home/matheus/apache-base
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-ant shutdown
-ant default
-ant startup
